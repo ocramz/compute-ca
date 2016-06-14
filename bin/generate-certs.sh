1a06ef67cb2f2bb440d5846aedf6d6ea5e58f821
ocramz <zocca.marco@gmail.com>
2016-06-14 19:29:55 +0200

    generate-certs.sh for provisioning variable # of nodes

--- bin/generate-certs.sh
+++ bin/generate-certs.sh
@@ -37,10 +37,30 @@ openssl rsa -in swarm-mgr-priv-key.pem -out swarm-mgr-priv-key.pem
 
 
 
+# ======  login node key pair
+
+# private key  ca-priv-key.pem :
+openssl genrsa -out login-priv-key.pem 2048
+
+
+# certificate signing request (CSR) swarm-mgr.csr using the private key 
+openssl req -subj "/CN=login" -new -key login-priv-key.pem -out login.csr
+
+# public key ":
+
+# openssl req -config /usr/lib/ssl/openssl.cnf -new -key swarm-mgr-priv-key.pem -x509 -days 1825 -subj '/C=US/ST=Oregon/L=Portland/O=IT/CN=swarm-mgr' -out swarm-mgr.pem
+
+openssl x509 -req -days 1825 -in login.csr -CA ca.pem -CAkey ca-priv-key.pem -CAcreateserial -out login-cert.pem -extensions v3_req -extfile /usr/lib/ssl/openssl.cnf
+
+openssl rsa -in login-priv-key.pem -out login-priv-key.pem
 
 
 
-for ii in `seq 1 $NNODES`
+
+
+
+
+for ii in swarm login `seq 1 $NNODES`
   do
     echo "Hello from node $ii\n"
   done
