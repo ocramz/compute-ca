6e3785a7bf4ce0bd55596661988f1629a19b04c6
ocramz <zocca.marco@gmail.com>
2016-06-14 19:10:44 +0200

    generate-certs.sh for provisioning variable # of nodes

--- bin/generate-certs.sh
+++ bin/generate-certs.sh
@@ -7,12 +7,12 @@ NNODES=$1
 
 openssl genrsa -out ca-priv-key.pem 2048
 
-# public key :
+# public key for the CA :
 
 openssl req -config /usr/lib/ssl/openssl.cnf -new -key ca-priv-key.pem -x509 -days 1825 -subj '/C=US/ST=Oregon/L=Portland/O=IT/CN=www.example.com' -out ca.pem
 
 
-for ii in {1..${NNODES}}
-do
-    echo "Hello from node $ii"
-done
+for ii in {1..$NNODES}
+  do
+    echo "Hello from node $ii\n"
+  done
