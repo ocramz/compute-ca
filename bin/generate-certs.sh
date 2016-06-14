518f12d0d3d17c601401d7aa4b155cdab081fce2
ocramz <zocca.marco@gmail.com>
2016-06-14 19:21:59 +0200

    generate-certs.sh for provisioning variable # of nodes

--- bin/generate-certs.sh
+++ bin/generate-certs.sh
@@ -3,16 +3,44 @@
 # of compute nodes
 NNODES=$1
 
-# private key called ca-priv-key.pem for the CA:
 
+# ====== CA key pair
+
+# private key ca-priv-key.pem :
 openssl genrsa -out ca-priv-key.pem 2048
 
-# public key for the CA :
+# public key " :
+openssl req -config /usr/lib/ssl/openssl.cnf -new -key ca-priv-key.pem -x509 -days 1825 -subj '/C=US/ST=Oregon/L=Portland/O=IT/CN=ca' -out ca.pem
 
-openssl req -config /usr/lib/ssl/openssl.cnf -new -key ca-priv-key.pem -x509 -days 1825 -subj '/C=US/ST=Oregon/L=Portland/O=IT/CN=www.example.com' -out ca.pem
 
 
-for ii in {1..$NNODES}
+
+# ====== Swarm manager key pair
+
+# private key  ca-priv-key.pem :
+openssl genrsa -out swarm-mgr-priv-key.pem 2048
+
+
+# certificate signing request (CSR) swarm-mgr.csr using the private key 
+openssl req -subj "/CN=swarm-mgr" -new -key swarm-mgr-priv-key.pem -out swarm-mgr.csr
+
+# public key ":
+
+# openssl req -config /usr/lib/ssl/openssl.cnf -new -key swarm-mgr-priv-key.pem -x509 -days 1825 -subj '/C=US/ST=Oregon/L=Portland/O=IT/CN=swarm-mgr' -out swarm-mgr.pem
+
+openssl x509 -req -days 1825 -in swarm-mgr.csr -CA ca.pem -CAkey ca-priv-key.pem -CAcreateserial -out swarm-mgr-cert.pem -extensions v3_req -extfile /usr/lib/ssl/openssl.cnf
+
+openssl rsa -in swarm-mgr-priv-key.pem -out swarm-mgr-priv-key.pem
+
+
+
+
+
+
+
+
+
+for ii in `seq 1 $NNODES`
   do
     echo "Hello from node $ii\n"
   done
